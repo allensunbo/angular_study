eb532a69b8132ec64f3fb2917d6938129d613137
allensunbo <allensunbo@gmail.com>
2014-12-21 17:38:34 -0500

    add selection

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@ var app = angular.module('studyApp', ['bgDirectives', 'angularTreeview']);
 
 console.info('angular version = ' + angular.version.full);
 
-app.controller('StudyCtrl', function() {
+app.controller('StudyCtrl', function($scope) {
     var vm = this;
     vm.studyCategories = [{
         'name': 'Portfolio Study',
@@ -15,4 +15,10 @@ app.controller('StudyCtrl', function() {
             'name': 'market pf'
         }]
     }];
+
+    $scope.$watch('studyList.currentNode', function(newObj, oldObj) {
+        if ($scope.studyList && angular.isObject($scope.studyList.currentNode)) {
+            console.log($scope.studyList.currentNode);
+        }
+    }, false);
 });
