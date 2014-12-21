64375f54e7a33f5768fc97ffacd4a0e7680ce119
allensunbo <allensunbo@gmail.com>
2014-12-21 11:33:54 -0500

    use angular treeview

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,18 @@
-var app = angular.module('studyApp', []);
+var app = angular.module('studyApp', ['bgDirectives', 'angularTreeview']);
+
+console.info('angular version = ' + angular.version.full);
+
+app.controller('StudyCtrl', function() {
+    var vm = this;
+    vm.studyCategories = [{
+        'name': 'Portfolio Study',
+        'id': '0',
+        'items': [{
+            'id': '01',
+            'name': 'awesome Portfolio'
+        }, {
+            'id': '02',
+            'name': 'market pf'
+        }]
+    }];
+});
