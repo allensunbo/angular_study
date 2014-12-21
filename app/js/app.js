91f2b81edffa77c8c7827c79810f1a93c6a1229f
allensunbo <allensunbo@gmail.com>
2014-12-21 18:26:15 -0500

    ui router

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,8 @@
-var app = angular.module('studyApp', ['bgDirectives', 'angularTreeview']);
+var app = angular.module('studyApp', ['bgDirectives', 'angularTreeview', 'ui.router']);
 
 console.info('angular version = ' + angular.version.full);
 
-app.controller('StudyCtrl', function($scope) {
+app.controller('StudyCtrl', function($scope, $location) {
     var vm = this;
     vm.studyCategories = [{
         'name': 'Portfolio Study',
@@ -19,6 +19,30 @@ app.controller('StudyCtrl', function($scope) {
     $scope.$watch('studyList.currentNode', function(newObj, oldObj) {
         if ($scope.studyList && angular.isObject($scope.studyList.currentNode)) {
             console.log($scope.studyList.currentNode);
+            $location.path('/route1/' + $scope.studyList.currentNode.name);
         }
     }, false);
 });
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+    // For any unmatched url, send to /route1
+    $urlRouterProvider.otherwise("/route1")
+
+    $stateProvider
+        .state('route1', {
+            url: "/route1/:pf",
+            template: "<div ng-controller=\"PITSummaryCtrl as pitSum\"><h1>{{pitSum.message}} {{location}}</h1></div>",
+            controller: function($scope, $stateParams) {
+                $scope.location = $stateParams.pf;
+            }
+        })
+        .state('route2', {
+            url: "/route2/:pf",
+            template: "<div ng-controller=\"PITSummaryCtrl as pitSum\"><h1>{{pitSum.message}} {{location}}</h1></div>",
+            controller: function($scope, $stateParams) {
+                $scope.location = $stateParams.pf;
+            }
+        })
+
+})
